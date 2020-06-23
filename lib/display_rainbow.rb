e844229b8c8bc6229c3da891db9e75fb42308991
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-23 18:03:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,6 @@
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow
+def display_rainbow(colors)
   puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
